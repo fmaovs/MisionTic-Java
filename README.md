d68a8b91ba81e64b9791372ac03744fd6aa04a55
Mauricio Villanueva <freddymauriciovillanuevasilva@gmail.com>
2022-07-13 22:04:57 -0500

    add readme description

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# MisionTic-Java
+# Mision Tic
+
+#### MisionTic is a governamental program design to train programmers fullstack
+[Mision Tic] (https://www.misiontic2022.gov.co/portal/)
+
+# Challenge 1
+Challenge is a practice exercise where students test our knowledge in the language we are learning.
+In cicle 2 we are learning Java, guide by the University of the North [UniNorte](https://uninorte.edu.co/)
+
+In this challenge a situation arises in which it is attempted to describe the resource of drinking water in a equitable manner for all, for this it is necessary to analyze the risk index of water quality. The program has to obtain the data of the samples in a list and find an average, the maximum value and the minimum value and compare them with a classification table and print their respective messages with the healt risk status.
+
+
+
--- reto1.java
+++ reto1.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package reto1;
 
 import java.util.Arrays;
@@ -19,98 +16,88 @@ public class Reto1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
+
         var averageRange = 0.00;
         var maxRange = 0.00;
         var minRange = 0.00;
-        
+
         Scanner sc = new Scanner(System.in);
-        int n;
-        n=sc.nextInt();
-        int[] numeros = new int[n];
-        //System.out.println(" El valor de n es: " + n );
-        
-        for (int i = 0 ; i < n ; i++){
-            //System.out.println(i);
-            numeros[i] = sc.nextInt();
+        int size;
+        size = sc.nextInt();
+        int[] samples = new int[size];
+        // System.out.println(" El valor de n es: " + n );
+
+        for (int i = 0; i < size; i++) {
+            // System.out.println(i);
+            samples[i] = sc.nextInt();
         }
 
-        //Get average
+        // Get average
         OptionalDouble optNumber;
-        optNumber = Arrays.stream(numeros).average();
-        
-        if(!optNumber.isEmpty()){
+        optNumber = Arrays.stream(samples).average();
+
+        if (!optNumber.isEmpty()) {
             var average = optNumber.getAsDouble();
             averageRange = average;
         }
-        
-        //Get maximum
+
+        // Get maximum
         OptionalInt optNumberMax;
-        optNumberMax = Arrays.stream(numeros).max();
-        
-        if(!optNumberMax.isEmpty()){
+        optNumberMax = Arrays.stream(samples).max();
+
+        if (!optNumberMax.isEmpty()) {
             var max = optNumberMax.getAsInt();
             maxRange = max;
         }
-        
-        //Get minimum
+
+        // Get minimum
         OptionalInt optNumberMin;
-        optNumberMin= Arrays.stream(numeros).min();
-        
-        if(!optNumberMin.isEmpty()){
+        optNumberMin = Arrays.stream(samples).min();
+
+        if (!optNumberMin.isEmpty()) {
             var min = optNumberMin.getAsInt();
             minRange = min;
         }
-        
-        //promedio
-        //System.out.println(averageRange);
-        //System.out.println(maxRange);
-        //System.out.println(minRange);
-        
-        
-        // Promedio Nivel de riesgo
-        if(averageRange >= 0.00 && averageRange <= 5.00) {
+
+        // Average risk level
+        if (averageRange >= 0.00 && averageRange <= 5.00) {
             System.out.println("SIN RIESGO");
-        } else  if(averageRange > 5 && averageRange <= 14) {
-                    System.out.println("BAJO");
-        } else  if(averageRange > 14 && averageRange <= 35) {
-                    System.out.println("MEDIO");
-        } else  if(averageRange > 35 && averageRange <= 80) {
-                    System.out.println("ALTO");
-        } else  if(averageRange > 80 && averageRange <=100) {
-                    System.out.println("INVIABLE SANITARIAMENTE");
+        } else if (averageRange > 5 && averageRange <= 14) {
+            System.out.println("BAJO");
+        } else if (averageRange > 14 && averageRange <= 35) {
+            System.out.println("MEDIO");
+        } else if (averageRange > 35 && averageRange <= 80) {
+            System.out.println("ALTO");
+        } else if (averageRange > 80 && averageRange <= 100) {
+            System.out.println("INVIABLE SANITARIAMENTE");
         }
-        
-        // max Nivel de riesgo}
-        
-        if(maxRange >= 0.00 && maxRange <= 5.00) {
+
+        // Maximum risk level
+
+        if (maxRange >= 0.00 && maxRange <= 5.00) {
             System.out.println("SIN RIESGO");
-        } else  if(maxRange > 5 && maxRange <= 14) {
-                    System.out.println("BAJO");
-        } else  if(maxRange > 14 && maxRange <= 35) {
-                    System.out.println("MEDIO");
-        } else  if(maxRange > 35 && maxRange <= 80) {
-                    System.out.println("ALTO");
-        } else  if(maxRange > 80 && maxRange <=100) {
-                    System.out.println("INVIABLE SANITARIAMENTE");
+        } else if (maxRange > 5 && maxRange <= 14) {
+            System.out.println("BAJO");
+        } else if (maxRange > 14 && maxRange <= 35) {
+            System.out.println("MEDIO");
+        } else if (maxRange > 35 && maxRange <= 80) {
+            System.out.println("ALTO");
+        } else if (maxRange > 80 && maxRange <= 100) {
+            System.out.println("INVIABLE SANITARIAMENTE");
         }
-        
-        // min Nivel de riesgo
-        if(minRange >= 0.00 && minRange <= 5.00) {
+
+        // Mimimun risk level
+        if (minRange >= 0.00 && minRange <= 5.00) {
             System.out.println("SIN RIESGO");
-        } else  if(minRange > 5 && minRange <= 14) {
-                    System.out.println("BAJO");
-        } else  if(minRange > 14 && minRange <= 35) {
-                    System.out.println("MEDIO");
-        } else  if(minRange > 35 && minRange <= 80) {
-                    System.out.println("ALTO");
-        } else  if(minRange > 80 && minRange <=100) {
-                    System.out.println("INVIABLE SANITARIAMENTE");
+        } else if (minRange > 5 && minRange <= 14) {
+            System.out.println("BAJO");
+        } else if (minRange > 14 && minRange <= 35) {
+            System.out.println("MEDIO");
+        } else if (minRange > 35 && minRange <= 80) {
+            System.out.println("ALTO");
+        } else if (minRange > 80 && minRange <= 100) {
+            System.out.println("INVIABLE SANITARIAMENTE");
         }
-                        
-        
+
     }
 }
-
-
